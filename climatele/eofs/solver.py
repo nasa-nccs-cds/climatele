e7b151612fa0db8f7ede3b6d0a35c83df268f346
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2018-06-07 12:39:14 -0400

    Add files

---
+++ climatele/eofs/solver.py
@@ -0,0 +1,59 @@
+from .cdms import Eof
+import cdutil, os
+import os.path
+import cdms2 as cdms
+import cdtime, math
+
+class EOFSolver:
+    variable = None  # type: cdms.AbstractVariable
+
+    def __init__(self, _project, _experiment, _outDir ):
+        self.project = _project
+        self.experiment = _experiment
+        self.directory = os.path.join( _outDir, self.project )
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+
+    def compute(self, data_variable, nModes, center=True, scale=True, removeCycle=True ):
+        self.variable = data_variable
+        data = cdutil.ANNUALCYCLE.departures(self.variable) if removeCycle else self.variable
+        self.solver = Eof( data, weights='none', center=center, scale=scale )
+        self.nModes = nModes
+        print "Created solver"
+
+        self.eofs = self.solver.eofs( neofs=nModes )
+        self.pcs = self.solver.pcs().transpose()
+        print "Computed eofs"
+
+        self.fracs = self.solver.varianceFraction()
+        self.pves = [ str(round(float(frac*100.),1)) + '%' for frac in self.fracs ]
+        self.save_results()
+        print "Saved results"
+
+    def save_results(self):
+        self.savePCs()
+        self.saveEOFs()
+
+    def savePCs(self):
+        outfilePath = os.path.join( self.directory, self.experiment + '-PCs.nc' )
+        outfile = cdms.open(outfilePath, 'w')
+        timeAxis = self.variable.getTime()
+
+        for iPlot in range(self.nModes):
+            pc = self.pcs[iPlot]  # type: cdms.Variable
+            plot_title_str = 'PC-' + str(iPlot) + ',' + self.experiment + ', ' + self.pves[iPlot]
+            v = cdms.createVariable(pc.data, None, 0, 0, None, float('nan'), None, [timeAxis],  {"pve": self.pves[iPlot], "long_name": plot_title_str}, "PC-" + str(iPlot))
+            outfile.write(v)
+        outfile.close()
+
+    def saveEOFs(self):
+        outfilePath = os.path.join( self.directory, self.experiment + '-EOFs.nc' )
+        outfile = cdms.open(outfilePath, 'w')
+        axes = [ self.variable.getLatitude(), self.variable.getLongitude() ]
+
+        for iPlot in range(self.nModes):
+            eof = self.eofs[iPlot]  # type: cdms.Variable
+            plot_title_str = 'EOF-' + str(iPlot) + ',' + self.experiment + ', ' + self.pves[iPlot]
+            v = cdms.createVariable(eof.data, None, 0, 0, None, float('nan'), None, axes,  {"pve": self.pves[iPlot], "long_name": plot_title_str}, "EOF-" + str(iPlot))
+            outfile.write(v)
+        outfile.close()
