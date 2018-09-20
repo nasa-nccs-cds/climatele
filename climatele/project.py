e84ab6b97c5c2116147fe310d833442ff647dac2
Thomas Maxwell <thomas.maxwell@nasa.gov>
2018-09-20 16:52:32 -0400

    Update Archive paths

--- climatele/project.py
+++ climatele/project.py
@@ -8,18 +8,18 @@ class Project:
 
     def __init__(self, baseDir, _name ):
         self.name = _name
-        self.exts = { PC: '-PCs.nc', EOF: '-EOFs.nc' }
+        self.exts = { PC: 'pcs.nc', EOF: 'eofs.nc' }
         self.directory = os.path.join( baseDir, _name )
         if not os.path.exists(self.directory):
             os.makedirs(self.directory)
 
     def outfilePath(self, experiment, rType ):
         # type: (str, int) -> str
-        return os.path.join( self.directory, experiment + self.exts.get( rType, "Invalid response type: " + str(rType) ) )
+        return os.path.join( self.directory, experiment, self.exts.get( rType, "Invalid response type: " + str(rType) ) )
 
     def getVariableNames(self, experiment, rType ):
         # type: (str, int) -> list[str]
-        dataset = self.getDataset( experiment, rType )
+        dataset = self.getDataset( experiment,  rType )
         return [ vname for vname in dataset.listvariables() if not vname.endswith("_bnds") ]
 
     def getDataset(self, experiment, rType ):
